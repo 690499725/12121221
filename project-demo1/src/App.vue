7042318d921088b526924e8b8babafeb3610cc36
itxiaofu <690499725@qq.com>
2025-08-26 18:52:59 +0800

    提交

---
+++ project-demo1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <router-view />
+  </div>
+</template>
+<script setup lang="ts">
+import {ref,reactive} from "vue"
+
+</script>
+<style>
+.el-menu {
+  border-right: none !important;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ project-demo1/src/views/layout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="layout-container">
+        <SliderNavs />
+        <div class="right-container" >
+            <TopNavs />
+            <div class="content">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup >
+import {ref,reactive} from "vue"
+import SliderNavs from "@/components/SliderNavs/index.vue"
+import TopNavs from "@/components/TopNavs/index.vue"
+
+
+</script>
+<style  scoped>
+.right-container {
+  margin-left: 250px; /* 保持与侧边栏宽度一致 */
+  transition: 0.3s ease-in;
+  width: calc(100% - 250px); /* 确保右侧内容区域宽度正确 */
+}
+.content {
+  padding: 10px;
+  overflow-x: hidden; 
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+</style>
